d6257b62668742f2e63d5fee9b18613f0786ed17
Ben Atkins <batkinz@gmail.com>
2015-07-14 02:27:07 -0400

    Add Skycon to the CurrentConditions component

--- js/components/current_conditions.js
+++ js/components/current_conditions.js
@@ -1,25 +1,38 @@
 import React   from 'react';
 import numeral from 'numeral';
+import Skycons from 'react-skycons';
 
 const {
   Component,
 } = React;
 
 class CurrentConditions extends Component {
+  skyconIconName() {
+    if (this.props.icon === undefined) {
+      return null;
+    }
+    let value = this.props.icon.toUpperCase();
+    return value.replace(/-/g, '_');
+  }
+
   render () {
     // Don't print until props are passed down
     if (this.props.temperature !== undefined) {
       return (
         <div id='current-conditions'>
-          <div className='temp'>
-            <div>
-              <span className='value'>
-                {Math.round(this.props.temperature)}
-              </span>
-              <span clasName='degree-symbol'>
-                &deg;
-              </span>
+          <div className='temp-and-skycon'>
+            <Skycons className='skycon' color='black' icon={this.skyconIconName()} />
+            <div className='temp'>
+              <div>
+                <span className='value'>
+                  {Math.round(this.props.temperature)}
+                </span>
+                <span clasName='degree-symbol'>
+                  &deg;
+                </span>
+              </div>
             </div>
+            <div className='clear'></div>
           </div>
           <div className='summary'>
             {this.props.summary}
