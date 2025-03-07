2841b8a1cd74f1599bc0da2135c816acbe893b0d
Ben Atkins <bdubs33@gmail.com>
2025-03-07 18:05:12 -0500

    Convert Weather and LocationBar into function components

--- js/components/location_bar.js
+++ js/components/location_bar.js
@@ -1,17 +1,11 @@
 import React from 'react';
 
-const {
-  Component,
-} = React;
-
-class LocationBar extends Component {
-  render () {
-    return (
-      <div id='location-bar'>
-        <h3>Weather for {this.props.location}</h3>
-      </div>
-    );
-  }
+const LocationBar = (props) => {
+  return (
+    <div id='location-bar'>
+      <h3>Weather for {props.location}</h3>
+    </div>
+  );
 };
 
 export default LocationBar;
--- js/components/weather.js
+++ js/components/weather.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import LocationBar       from './location_bar';
 import CurrentConditions from './current_conditions';
@@ -6,68 +6,63 @@ import WeekOverview      from './week_overview';
 
 import getForecast from './../utils/get_forecast';
 
-const {
-  Component,
-} = React;
-
 import './../../styles.scss';
 
 import config from './../../config.json';
 
-class Weather extends Component {
-  constructor (props) {
-    super(props);
+const Weather = () => {
+  const [location, setLocation] = useState(config.location);
+  const [hourly, setHourly] = useState();
+  const [daily, setDaily] = useState();
+  const [currently, setCurrently] = useState();
 
-    this.state = { location: config.location };
-
-    if (config.api_key !== null) {
-      this.fetchCurentForecast();
-    }
+  const fetchCurentForecast = () => {
+    getForecast(handleForecastResponse);
   }
 
-  fetchCurentForecast() {
-    getForecast(this.handleForecastResponse.bind(this));
-  }
-
-  handleForecastResponse(err, res) {
+  const handleForecastResponse = (err, res) => {
     if (res.body !== undefined) {
-      this.setState(res.body);
+      setCurrently(res.body.currently);
+      setDaily(res.body.daily);
+      setHourly(res.body.hourly);
     } else {
       console.log('Error!');
       console.log(err);
     }
   }
 
-  nextTemp () {
-    if (this.state.hourly === undefined) {
+  const nextTemp = () => {
+    if (hourly === undefined) {
       return undefined;
     }
 
-    return this.state.hourly.data[0].temperature;
+    return hourly.data[0].temperature;
   }
 
-  render () {
-    if (config.api_key !== null) {
-      return (
-        <div>
-          <LocationBar location={this.state.location} />
-          <CurrentConditions nextTemp={this.nextTemp()} {...this.state.currently} />
-          <WeekOverview {...this.state.daily} />
-          <div className='clear'></div>
-        </div>
-      );
-    }
-    else {
-      return (
-        <div>
-          Please enter your API Key for
-          {' '}
-          <a href='https://developer.forecast.io/' target='_blank'>Forecast.io</a>
-          {' '}
-          in <code>config.json</code> in the project root directory
-        </div>
-      )
-    }
+  if (config.api_key !== null && currently === undefined) {
+    fetchCurentForecast();
+  }
+
+  if (config.api_key !== null) {
+    return (
+      <div>
+        <LocationBar location={location} />
+        <CurrentConditions nextTemp={nextTemp()} {...currently} />
+        <WeekOverview {...daily} />
+        <div className='clear'></div>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div>
+        Please enter your API Key for
+        {' '}
+        <a href='https://developer.forecast.io/' target='_blank'>Forecast.io</a>
+        {' '}
+        in <code>config.json</code> in the project root directory
+      </div>
+    )
   }
 };
 
