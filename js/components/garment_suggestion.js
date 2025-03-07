c84adce0388725a24cc832c39cdca37923f4a1c1
Ben Atkins <bdubs33@gmail.com>
2025-03-07 18:11:30 -0500

    Convert GarmentSuggestion to function component

--- js/components/garment_suggestion.js
+++ js/components/garment_suggestion.js
@@ -2,22 +2,15 @@ import React from 'react';
 
 import suggestGarment from './../utils/suggest_garment';
 
-const {
-  Component,
-} = React;
-
-class GarmentSuggestion extends Component {
-  render () {
-    if (this.props.temperature !== undefined) {
-      return (
-        <div className='garment-suggestion'>
-          {suggestGarment(this.props.temperature, this.props.activity)}
-        </div>
-      );
-    }
-    else {
-      return null;
-    }
+const GarmentSuggestion = (props) => {
+  if (props.temperature !== undefined) {
+    return (
+      <div className='garment-suggestion'>
+        {suggestGarment(props.temperature, props.activity)}
+      </div>
+    );
+  } else {
+    return null;
   }
 };
 
