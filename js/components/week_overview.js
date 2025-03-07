47d3516f5e4598aa42daad263513c1a990c95b8e
Ben Atkins <bdubs33@gmail.com>
2025-03-07 18:09:05 -0500

    Convert WeekOverview to function component

--- js/components/week_overview.js
+++ js/components/week_overview.js
@@ -2,40 +2,30 @@ import React from 'react';
 
 import DayOverview from './day_overview';
 
-const {
-  Component,
-} = React;
-
-class WeekOverview extends Component {
-  contents() {
-    if (this.props.data === undefined) {
-      return null;
-    }
-
-    return(this.props.data.map((data, i) => {
+const WeekOverview = (props) => {
+  const contents = () => {
+    return(props.data?.map((data, i) => {
       return( <DayOverview key={i} {...data} /> );
     }));
   }
 
-  render () {
-    if (this.props.data !== undefined) {
-      return (
-        <div id='week-overview'>
-          <div className='labels'>
-            <div className='high'>High</div>
-            <div className='low'>Low</div>
-            <div className='garment'>Garment</div>
-          </div>
-          <div className='days'>
-            {this.contents()}
-          </div>
-          <div className='clear'></div>
+  if (props.data !== undefined) {
+    return (
+      <div id='week-overview'>
+        <div className='labels'>
+          <div className='high'>High</div>
+          <div className='low'>Low</div>
+          <div className='garment'>Garment</div>
         </div>
-      );
-    }
-    else {
-      return null;
-    }
+        <div className='days'>
+          {contents()}
+        </div>
+        <div className='clear'></div>
+      </div>
+    );
+  }
+  else {
+    return null;
   }
 };
 
