15e2acef233fbb7dc94ac875f878d5ca654fd373
Ben Atkins <bdubs33@gmail.com>
2025-03-07 18:13:16 -0500

    Convert DayOverview to function component

--- js/components/day_overview.js
+++ js/components/day_overview.js
@@ -6,40 +6,33 @@ import GarmentSuggestion from './garment_suggestion';
 
 import translateSkyconIconName from './../utils/translate_skycon_icon_name';
 
-const {
-  Component,
-} = React;
-
-class DayOverview extends Component {
-  render () {
-    if (this.props.time !== undefined) {
-      return (
-        <div className='day'>
-          <div className='day-of-week'>
-            {moment(this.props.time, 'X').format('ddd')}
-          </div>
-          <Skycons className = 'skycon'
-                   color     = 'black'
-                   type      = {translateSkyconIconName(this.props.icon)} />
-          <div className='temps'>
-            <div className='high'>
-              {Math.round(this.props.temperatureMax)}
-              &deg;
-            </div>
-            <div className='low'>
-              {Math.round(this.props.temperatureMin)}
-              &deg;
-            </div>
-          </div>
-          <GarmentSuggestion
-              temperature = {this.props.temperatureMax}
-              activity    = {this.props.summary} />
+const DayOverview = (props) => {
+  if (props.time !== undefined) {
+    return (
+      <div className='day'>
+        <div className='day-of-week'>
+          {moment(props.time, 'X').format('ddd')}
         </div>
-      );
-    }
-    else {
-      return null;
-    }
+        <Skycons className = 'skycon'
+                 color     = 'black'
+                 type      = {translateSkyconIconName(props.icon)} />
+        <div className='temps'>
+          <div className='high'>
+            {Math.round(props.temperatureMax)}
+            &deg;
+          </div>
+          <div className='low'>
+            {Math.round(props.temperatureMin)}
+            &deg;
+          </div>
+        </div>
+        <GarmentSuggestion
+            temperature = {props.temperatureMax}
+            activity    = {props.summary} />
+      </div>
+    );
+  } else {
+    return null;
   }
 };
 
