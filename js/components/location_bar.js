995a0fb8287b70d22e9f93aa7695d39e576ee973
Ben Atkins <bdubs33@gmail.com>
2025-03-14 21:48:25 -0400

    Ignore js files since project is entirely converted to use Typescript

--- js/components/location_bar.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = LocationBar;
-const jsx_runtime_1 = require("react/jsx-runtime");
-function LocationBar({ location }) {
-    return ((0, jsx_runtime_1.jsx)("div", { id: 'location-bar', children: (0, jsx_runtime_1.jsxs)("h3", { children: ["Weather for ", location] }) }));
-}
--- js/utils/translate_skycon_icon_name.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = default_1;
-// Takes an icon name format returned from Forecast.IO and converts it
-// a format that is consumable by the Skycons npm library
-function default_1(forecastIconName) {
-    return forecastIconName?.toUpperCase()?.replace(/-/g, '_');
-}
-;
